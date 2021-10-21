6c2b653866e9283bafd34959b0c1443b70d0589e
Leonardo <contato.prof.leonardo@gmail.com>
2021-10-20 21:52:04 -0400

    exercícios do uri

---
+++ uri1008.py
@@ -0,0 +1,8 @@
+numero = int(input())
+qnt_horas_trabalhadas = int(input())
+salario_hora = float(input())
+
+salario = salario_hora * qnt_horas_trabalhadas
+
+print(f'NUMBER = {numero}')
+print(f'SALARY = U$ {salario:.2f}')
